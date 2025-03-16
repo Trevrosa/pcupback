813c9909ba33d76ff1e751923cceec79682c1713
trev <trev@trevrosa.dev>
2025-03-16 14:38:14 +0800

    ddd

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -4,14 +4,14 @@ use syn::parse_quote;
 
 /// A test harness that inserts the `#[test]` attribute and provides the `Client` through `crate::test_rocket(#fn_name)`.
 #[proc_macro_attribute]
-pub fn rocket_test(_: TokenStream, item: TokenStream) -> TokenStream {
+pub fn rocket_test(_args: TokenStream, item: TokenStream) -> TokenStream {
     // item should be a function
     let mut input = syn::parse_macro_input!(item as syn::ItemFn);
 
     let fn_name = input.sig.ident.to_string();
 
     let client_decl = parse_quote! {
-        let client = rocket::local::blocking::Client::tracked(crate::test_rocket(#fn_name)).unwrap();
+        let client = ::rocket::local::blocking::Client::tracked(crate::test_rocket(#fn_name)).unwrap();
     };
     let test_attr = parse_quote!(#[test]);
 
